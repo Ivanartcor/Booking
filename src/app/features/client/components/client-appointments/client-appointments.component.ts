9ba8e93c2dd4d1c511b5a7c48d4a36de07664dca
Iván <ivanartcor@gmail.com>
2024-12-03 22:28:40 +0100

    ventana emergente citas

--- src/app/features/client/components/client-appointments/client-appointments.component.ts
+++ src/app/features/client/components/client-appointments/client-appointments.component.ts
@@ -8,7 +8,37 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class ClientAppointmentsComponent {
   @Output() close = new EventEmitter<void>();
 
+  appointments = [
+    {
+      id: 1,
+      company: 'Empresa',
+      service: 'Corte de pelo',
+      price: 12.0,
+      date: new Date(2024, 8, 17), // Año, Mes (0-indexado), Día
+      time: '10:15'
+    },
+    {
+      id: 2,
+      company: 'Empresa',
+      service: 'Corte de pelo',
+      price: 12.0,
+      date: new Date(2024, 8, 17),
+      time: '10:15'
+    },
+    {
+      id: 3,
+      company: 'Empresa',
+      service: 'Corte de pelo',
+      price: 12.0,
+      date: new Date(2024, 8, 17),
+      time: '10:15'
+    }
+  ];
   closeModal() {
     this.close.emit(); // Emite un evento para notificar el cierre
+  }
+  cancelAppointment(id: number) {
+    // Lógica para cancelar una cita (de momento, solo remueve de la lista)
+    this.appointments = this.appointments.filter(appointment => appointment.id !== id);
   }
 }
