76d094d0702435c874e86cca95518a0bd8a5ee1e
Isaac <corderogarcia4496@gmail.com>
2024-12-11 08:53:14 +0100

    .

--- src/app/features/client/components/company-details/company-details.component.ts
+++ src/app/features/client/components/company-details/company-details.component.ts
@@ -14,7 +14,12 @@ export class CompanyDetailsComponent implements OnInit {
     {
       id: 1,
       name: 'Peluquería Lola',
-      description: 'Servicios de cuidado capilar orgánico.',
+      description: `
+        En un rincón especializado lleno de amabilidad y estilo, Peluquería Lola destaca por su atención personalizada. 
+        Ofrecemos servicios de calidad para el cuidado del cabello, con productos 100% orgánicos y profesionales altamente capacitados. 
+        Disfruta de cortes modernos, peinados para ocasiones especiales, tintes personalizados y tratamientos capilares avanzados. 
+        Además, garantizamos un ambiente cómodo y relajante. ¡Reserva tu cita ahora y vive una experiencia única en el cuidado de tu cabello!
+      `,
       services: [
         { id: 1, name: 'Corte de pelo', price: 12.0, reserved: false },
         { id: 2, name: 'Tinte', price: 25.0, reserved: false },
@@ -24,7 +29,13 @@ export class CompanyDetailsComponent implements OnInit {
     {
       id: 2,
       name: 'Vitaldent',
-      description: 'Cuida tu sonrisa con tecnología avanzada.',
+      description: `
+        Tu sonrisa es nuestra prioridad. Con tecnología avanzada y un equipo experto, Vitaldent ofrece servicios de alta calidad en odontología. 
+        Desde limpiezas dentales de rutina hasta tratamientos complejos como implantes y ortodoncia, estamos comprometidos con tu bienestar dental. 
+        Nuestros servicios incluyen diagnósticos digitales de última generación, planes de tratamiento personalizados y un ambiente acogedor 
+        diseñado para minimizar cualquier ansiedad relacionada con el dentista. 
+        ¡Reserva tu cita hoy y da el primer paso hacia una sonrisa saludable y brillante!
+      `,
       services: [
         { id: 1, name: 'Limpieza dental', price: 30.0, reserved: false },
         { id: 2, name: 'Implante', price: 1200.0, reserved: false },
