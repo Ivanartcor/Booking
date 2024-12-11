37611d80e541e9231504f8baf194155425196b57
Javigarcilop <110486658+Javigarcilop@users.noreply.github.com>
2024-12-11 09:56:17 +0100

    commit

--- src/app/features/company/components/company-dashboard/company-dashboard.component.ts
+++ src/app/features/company/components/company-dashboard/company-dashboard.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./company-dashboard.component.scss']
 })
 export class CompanyDashboardComponent {
-
+  // Array de servicios
+  services = [
+    {
+      title: 'Consulta Médica',
+      description: 'Consulta con médicos especializados en diversas áreas para atender tus necesidades de salud.',
+      imageUrl: '/assets/images/service1.jpg'
+    },
+    {
+      title: 'Urgencias',
+      description: 'Atención inmediata para casos de urgencia y emergencias médicas las 24 horas del día.',
+      imageUrl: '/assets/images/service2.jpg'
+    },
+    {
+      title: 'Consultas a Domicilio',
+      description: 'Ofrecemos consultas médicas en la comodidad de tu hogar, para mayor comodidad y atención.',
+      imageUrl: '/assets/images/service3.jpg'
+    }
+  ];
 }
