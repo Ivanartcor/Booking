3f746881e254b04f5e6965b23446935dffbfe7f6
Javi García <javigarcilop@gmail.com>
2025-01-18 14:27:41 +0100

    commit

--- src/app/features/calendar/calendar.component.ts
+++ src/app/features/calendar/calendar.component.ts
@@ -1,9 +1,8 @@
-// src/app/calendar/calendar.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';  // Importar CalendarOptions
 import dayGridPlugin from '@fullcalendar/daygrid';  // Importar dayGridPlugin
 import interactionPlugin from '@fullcalendar/interaction';  // Importar interactionPlugin
+import esLocale from '@fullcalendar/core/locales/es';  // Importar la localización en español
 
 @Component({
   selector: 'app-calendar',
@@ -15,6 +14,7 @@ export class CalendarComponent implements OnInit {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',  // Vista inicial (mensual)
     plugins: [dayGridPlugin, interactionPlugin],  // Asegúrate de agregar interactionPlugin
+    locale: esLocale,  // Configurar el idioma a español
     events: [
       { title: 'Evento 1', date: '2025-01-20' },  // Evento de ejemplo
       { title: 'Evento 2', date: '2025-01-25' }
