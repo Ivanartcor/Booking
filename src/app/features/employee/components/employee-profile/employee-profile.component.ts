db73c4a2e209cb41d3c450ea7b2d243503450fbd
Javigarcilop <110486658+Javigarcilop@users.noreply.github.com>
2025-02-03 14:12:34 +0100

    commit

--- src/app/features/employee/components/employee-profile/employee-profile.component.ts
+++ src/app/features/employee/components/employee-profile/employee-profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/core/services/employee.service';
 
 @Component({
   selector: 'app-employee-profile',
   templateUrl: './employee-profile.component.html',
   styleUrls: ['./employee-profile.component.scss']
 })
-export class EmployeeProfileComponent {
+export class EmployeeProfileComponent implements OnInit {
+  employee: any;
 
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    const employeeId = 2;  // ID del empleado a mostrar (puedes hacerlo dinámico)
+    
+    this.employeeService.getEmployees().subscribe((employees) => {
+      this.employee = employees.find(emp => emp.id === employeeId);
+    });
+  }
 }
+
