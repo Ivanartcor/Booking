f2689a16532db19e5c475ddb1189467980c5be9a
Javigarcilop <110486658+Javigarcilop@users.noreply.github.com>
2025-02-11 13:56:42 +0100

    commit

--- src/app/features/contacto/contacto.component.ts
+++ src/app/features/contacto/contacto.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
   styleUrls: ['./contacto.component.scss']
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+  contactForm!: FormGroup;
 
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required]]
+    });
+  }
+
+  // Método de envío del formulario
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+      console.log('Formulario enviado:', formData);
+      alert('¡Gracias por tu mensaje!');
+      this.contactForm.reset();  // Limpiar el formulario después de enviarlo
+    } else {
+      alert('Por favor, completa el formulario correctamente.');
+    }
+  }
 }
