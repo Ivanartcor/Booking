5cb3a1af3f997d6a7d0b1de6479dec2fe8fe8ecf
isaaccg96 <corderogarcia4496@gmail.com>
2025-02-11 14:34:06 +0100

    .

--- src/app/features/faq/faq.component.ts
+++ src/app/features/faq/faq.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent {
+export class FaqComponent implements AfterViewInit {
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const buttons = this.el.nativeElement.querySelectorAll('.accordion-button');
+
+    buttons.forEach((button: HTMLElement) => {
+      this.renderer.listen(button, 'click', () => {
+        setTimeout(() => {
+          const expanded = button.getAttribute("aria-expanded") === "true";
+          if (expanded) {
+            button.scrollIntoView({ behavior: "smooth", block: "center" });
+          }
+        }, 300);
+      });
+    });
+  }
 }
