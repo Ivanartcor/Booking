80283195b3911037ce3ca846cc9d900c0b93dd4e
isaaccg96 <corderogarcia4496@gmail.com>
2025-02-24 14:43:52 +0100

    email contacto

--- src/app/features/contacto/contacto.component.ts
+++ src/app/features/contacto/contacto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contacto',
@@ -8,7 +9,6 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactoComponent implements OnInit {
   contactForm!: FormGroup;
-
 
   constructor(private fb: FormBuilder) {}
 
@@ -24,9 +24,27 @@ export class ContactoComponent implements OnInit {
   onSubmit(): void {
     if (this.contactForm.valid) {
       const formData = this.contactForm.value;
-      console.log('Formulario enviado:', formData);
-      alert('¡Gracias por tu mensaje!');
-      this.contactForm.reset();  // Limpiar el formulario después de enviarlo
+
+      const serviceId = 'service_dybp3av';  // Reemplaza con tu Service ID
+      const templateId = 'template_74ofzop';  // Reemplaza con tu Template ID
+      const publicKey = 'nM6UIy6BmBA_c7Nht';  // Reemplaza con tu Public Key
+
+      const templateParams = {
+        user_name: formData.nombre,
+        user_email: formData.email,
+        user_message: formData.mensaje
+      };
+
+      emailjs.send(serviceId, templateId, templateParams, publicKey)
+        .then(response => {
+          console.log('Correo enviado con éxito!', response);
+          alert('¡Mensaje enviado correctamente!');
+          this.contactForm.reset();
+        })
+        .catch(error => {
+          console.error('Error al enviar el correo:', error);
+          alert('Hubo un problema al enviar el mensaje. Inténtalo más tarde.');
+        });
     } else {
       alert('Por favor, completa el formulario correctamente.');
     }
